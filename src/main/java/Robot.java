8927e3b1e73748c4bb3ebafb4232d43b03335e83
JacobKaylor <jkaylor20@yahoo.com>
2018-02-09 14:58:58 -0500

    Robot rust test

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,5 +1,7 @@
 
 public class Robot extends VirtualPet{
+	
+	
 
 
 	public Robot(String name, String description, int boredom, int health) {
@@ -9,7 +11,13 @@ public class Robot extends VirtualPet{
 
 	public void oilAllRobots() {
 		setHealth(getHealth() + 15);
-	 
+	}
+	public void tick() {
+		setHealth(getHealth() - 2);
+	}
+	public boolean isOiled() {
+		return true;
+		
 	}
 
 
--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -3,15 +3,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class RobotTest {
-	
-	
+
 	@Test
-	
-	public void shouldAcceptOil() {
-	Robot underTest = new Robot("name", "description",50,50);
-	underTest.oilAllRobots();
-	int oil = underTest.getHealth();
-	assertEquals(oil, 65);
+	public void shouldAcceptOiling() {
+		Robot underTest = new Robot("name", "description", 50, 50);
+		underTest.oilAllRobots();
+		int oil = underTest.getHealth();
+		assertEquals(oil, 65);
+	}
+
+	@Test
+	public void shouldRust() {
+		Robot underTest = new Robot("name", "description", 50, 50);
+		underTest.tick();
+		int rust = underTest.getHealth();
+		assertEquals(rust, 50);
+
 	}
 
 }
