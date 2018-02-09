ff40cd906e159a0bc1d1074a506f8d70b9a7dc5a
JacobKaylor <jkaylor20@yahoo.com>
2018-02-09 16:48:30 -0500

    OrganicDog Test 1

--- src/test/java/OrganicDogTest.java
+++ src/test/java/OrganicDogTest.java
@@ -1,7 +1,20 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class OrganicDogTest {
 	@Test
-	
+	public void shouldIncreaseHappinessWhenWalked() {
+		OrganicDog underTest = new OrganicDog("","",50,50,50,50,50);
+		underTest.walk();
+		int walk = underTest.getHappiness();
+		assertEquals(walk, 60);
+	}
+	public void shouldDedcreaseWasteLevelWhenWalked() {
+		OrganicDog underTest = new OrganicDog("","",50,50,50,50,50);
+		underTest.walk();
+		int walk = underTest.getWaste();
+		assertEquals(walk, 35);
+	}
 
 }
